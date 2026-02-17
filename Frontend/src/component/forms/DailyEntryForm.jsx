7ec5545969cdd403b91d1a368051b54917118f13
natish g <natishg8@gmail.com>
2026-02-17 15:44:33 +0530

    Store feed data

--- Frontend/src/component/forms/DailyEntryForm.jsx
+++ Frontend/src/component/forms/DailyEntryForm.jsx
@@ -65,10 +65,10 @@ const DailyEntryForm = () => {
                     <input type='number' id='mortality' name='mortality' value={dailyEntry.mortality} onChange={handelChange} placeholder='Enter mortality' className='outline-2  outline-amber-600 rounded-lg p-2 ' />
 
                     <label htmlFor="feedcount" className='font-bold'>Feed</label>
-                    <input type='number' id='feedcount' name='feedcount' value={dailyEntry.feedcount} onChange={handelChange} placeholder='Enter feed' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
+                    <input type='number' required id='feedcount' name='feedcount' value={dailyEntry.feedcount} onChange={handelChange} placeholder='Enter feed' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
 
                     <label htmlFor="feedType" className='font-bold'>Feed type</label>
-                    <select id="feedType" name='feedtype' value={dailyEntry.feedtype} onChange={handelChange} className='outline-2 outline-amber-600 rounded-lg p-3 0'>
+                    <select id="feedType" required name='feedtype' value={dailyEntry.feedtype} onChange={handelChange} className='outline-2 outline-amber-600 rounded-lg p-3 0'>
                         <option value="">Select Type</option>
                         <option value="Pre-starter">Pre-starter</option>
                         <option value="Starter">Starter</option>
@@ -77,7 +77,7 @@ const DailyEntryForm = () => {
 
                     <label htmlFor="avgweight" className='font-bold'>Avg weight</label>
                     <div className='border-2 p-2 border-amber-600 rounded-lg flex justify-between'>
-                        <input type='number' id="avgweight" name='avgweight' value={dailyEntry.avgweight} onChange={handelChange} placeholder='Enter avg weight' className='outline-none w-full p-0' />
+                        <input type='number' required id="avgweight" name='avgweight' value={dailyEntry.avgweight} onChange={handelChange} placeholder='Enter avg weight' className='outline-none w-full p-0' />
                         <select id="unit" name='unit' value={dailyEntry.unit} onChange={handelChange} className='outline-none'>
                             <option value="">Select unit</option>
                             <option value="g">g</option>
@@ -87,11 +87,11 @@ const DailyEntryForm = () => {
 
                     <label htmlFor="createdAt" className='font-bold'>Date</label>
                     <div className='flex flex-col gap-2 border-2 p-2 w-full border-amber-600 rounded-lg justify-between'>
-                        <input type="date" id="createdAt" name='createdAt' value={dailyEntry.createdAt} onChange={handelChange} className='outline-none' />
+                        <input type="date" required id="createdAt" name='createdAt' value={dailyEntry.createdAt} onChange={handelChange} className='outline-none' />
                     </div>
 
                     <label htmlFor="feedback" className='font-bold'>Feedback</label>
-                    <textarea type='number' id='feedback' name='feedback' value={dailyEntry.feedback} onChange={handelChange} placeholder='Enter feedback' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
+                    <textarea type='number' required id='feedback' name='feedback' value={dailyEntry.feedback} onChange={handelChange} placeholder='Enter feedback' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
 
                     <button type='submit' className='bg-amber-600 p-2 cursor-pointer rounded-lg text-[15px] font-bold text-white'>Submit</button>
                 </form>
--- Frontend/src/component/forms/FeedForm.jsx
+++ Frontend/src/component/forms/FeedForm.jsx
@@ -1,43 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from "../utils/api"
+import { useAuth } from '../context/AuthContext'
+import { toast } from 'react-toastify';
 
 const FeedForm = () => {
+
+    const {batch}=useAuth();
+    const [feed,setFeed]=useState({
+        feedpack:"",
+        feedcategory:"",
+        feedtype:"",
+        deliveredDate:"",
+        deliveredby:"",
+        vehicleno:""
+    })
+
+    const handelChange=(e)=>{
+        const {name,value}=e.target;
+        setFeed({
+            ...feed,
+            [name]:value
+        })
+    }
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try {
+            const token =localStorage.getItem("token");
+            if(token){
+                const res=await api.post("feed/addfeed",{
+                    ...feed,
+                    userid:batch._id,
+                    batchid:batch._id
+                })
+                if(res.data.success){
+                    console.log(res);
+                    toast.success("Feed added")
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
   return (
     
         <div className='max-h-screen p-5 mt-16 mb-5 flex justify-center items-center'>
-            <div className='w-100 shadow-lg p-3 rounded-xl'>
+            <div onSubmit={handleSubmit} className='w-100 shadow-lg p-3 rounded-xl'>
                 <p className='text-center text-2xl font-bold text-amber-700'><span className='text-4xl text-amber-800'>F</span>eed Entry</p>
-                <form className='flex flex-col gap-3 '>
-                    <label htmlFor="feed" className='font-bold'>Feed</label>
-                    <input type='number' id='feed' placeholder='Enter feed' className='outline-2  outline-amber-600 rounded-lg p-2 ' />
+                
+                <form  className='flex flex-col gap-3 '>
+                    
+                    <label htmlFor="feedpack" className='font-bold'>Feed</label>
+                    <input type='number' id='feedpack' required name='feedpack' value={feed.feedpack} onChange={handelChange} placeholder='Enter feed' className='outline-2  outline-amber-600 rounded-lg p-2 ' />
     
                     <label htmlFor="feedcategory" className='font-bold'>Feed category</label>
-                    <select id="feedcategory" className='outline-2 outline-amber-600 rounded-lg p-3 0'>
+                    <select id="feedcategory" required name='feedcategory' value={feed.feedcategory} onChange={handelChange} className='outline-2 outline-amber-600 rounded-lg p-3 0'>
                         <option value="">Select category</option>
-                        <option value="Pre-starter">New Feed</option>
-                        <option value="Starter">Return Feed</option>
+                        <option value="New feed">New Feed</option>
+                        <option value="Return feed">Return Feed</option>
                     </select>
 
                     <label htmlFor="feedType" className='font-bold'>Feed type</label>
-                    <select id="feedType" className='outline-2 outline-amber-600 rounded-lg p-3 0'>
+                    <select id="feedType" required name='feedtype' value={feed.feedtype} onChange={handelChange} className='outline-2 outline-amber-600 rounded-lg p-3 0'>
                         <option value="">Select Type</option>
                         <option value="Pre-starter">Pre-starter</option>
                         <option value="Starter">Starter</option>
                         <option value="Finisher">Finisher</option>
                     </select>
 
-                    <label htmlFor="date" className='font-bold'>Date</label>
+                    <label htmlFor="deliveredDate" className='font-bold'>Date</label>
                     <div className='flex flex-col gap-2 border-2 p-2 w-full border-amber-600 rounded-lg justify-between'>
-                    <input type="date" id="date" className='outline-none'/>
+                    <input type="date" required id="deliveredDate" name='deliveredDate' value={feed.deliveredDate} onChange={handelChange} className='outline-none'/>
                     </div>
 
                     <label htmlFor="deliveredby" className='font-bold'>Delivered by</label>
-                    <input type='text' id='deliveredby' placeholder='Enter name' className='outline-2  outline-amber-600 rounded-lg p-2 ' />
+                    <input type='text' required id='deliveredby' name='deliveredby' value={feed.deliveredby} onChange={handelChange} placeholder='Enter name' className='outline-2  outline-amber-600 rounded-lg p-2 ' />
 
                     <label htmlFor="vehicleno" className='font-bold'>Vehicle no</label>
-                    <input type='text' id='vehicleno' placeholder='Enter feedback' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
+                    <input type='text' required id='vehicleno' name='vehicleno' value={feed.vehicleno} onChange={handelChange} placeholder='Enter feedback' className='outline-2 outline-amber-600 rounded-lg p-2 ' />
 
-                    <button className='bg-amber-600 p-2 cursor-pointer rounded-lg text-[15px] font-bold text-white'>Submit</button>
+                    <button type='submit' className='bg-amber-600 p-2 cursor-pointer rounded-lg text-[15px] font-bold text-white'>Submit</button>
                 </form>
+
             </div>
         </div>
   )
